1e1914baa95e7470b58e1de539012de22c09e350
Ashish Rathore <ashishrathor783@gmail.com>
2023-04-07 00:11:14 +0530

    create new project

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import './style.css'
+
+function Home() {
+  return (
+    <Fragment>
+        <h1 className='frst'>this is react</h1>
+        <h2 style={{color:'greenyellow'}}>this a paragraph line</h2>
+        <p>sadfdfad safsfasdf saf saf asf safasf sf afasf sa sfsaf saf asfs</p>
+    </Fragment>
+  )
+}
+
+export default Home
