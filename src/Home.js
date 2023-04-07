b7a7d924b499263cb252c900ab8b9b2323fbac16
Harsh Tripathi <harshtripathi@Harshs-MacBook-Pro.local>
2023-04-07 22:39:14 +0530

    sada

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,19 @@ import './style.css'
 function Home() {
   return (
     <Fragment>
-        <h1 className='frst'>this is react</h1>
-        <h2 style={{color:'greenyellow'}}>this a paragraph line</h2>
-        <p>sadfdfad safsfasdf saf saf asf safasf sf afasf sa sfsaf saf asf cxxfdfsdgdgdg dgdsgs</p>
-        <h1>sdf</h1>
-        <h4>ashish rathor</h4>
-        <p className='text-primary'>this is from mac</p>
+      <h1 className='frst'>this is react</h1>
+      <h2 style={{ color: 'greenyellow' }}>this a paragraph line</h2>
+      <p>sadfdfad safsfasdf saf saf asf safasf sf afasf sa sfsaf saf asf cxxfdfsdgdgdg dgdsgs</p>
+      <h1>sdf</h1>
+      <h4>ashish rathor</h4>
+      <p className='text-primary'>this is from mac</p>
+      <div className="card" style="width: 18rem;">
+        <img src="logo.svg" className="card-img-top" alt="...">
+          <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+      </div>
+
     </Fragment>
   )
 }
