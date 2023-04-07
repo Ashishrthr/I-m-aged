b6f8f9c283ff9a8406ea2b63a6730d945b012004
Harsh Tripathi <harshtripathi@Harshs-MacBook-Pro.local>
2023-04-07 23:08:23 +0530

    add bootstrap 5

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
 import './style.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
 
 function Home() {
   return (
@@ -10,8 +12,8 @@ function Home() {
       <h1>sdf</h1>
       <h4>ashish rathor</h4>
       <p className='text-primary'>this is from mac</p>
-      <div className="card" style="width: 18rem;">
-        <img src="logo.svg" className="card-img-top" alt="...">
+  <div className="card" style={{width: '18rem' }}>
+        <img src="logo.svg" className="card-img-top" alt=""/>
           <div className="card-body">
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
