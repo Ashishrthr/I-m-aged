4e597d727a48011c0be787a0a35c3b30c58a95c5
Ashishrthr <ashishrathor783@gmail.com>
2023-04-09 21:59:16 +0530

    add navbar

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,46 @@ import './style.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 
+
 function Home() {
   return (
     <Fragment>
-      <h1 className='frst'>this is react</h1>
-      <h2 style={{ color: 'greenyellow' }}>this a paragraph line</h2>
-      <p>sadfdfad safsfasdf saf saf asf safasf sf afasf sa sfsaf saf asf cxxfdfsdgdgdg dgdsgs</p>
-      <h1>sdf</h1>
-      <h4>ashish rathor</h4>
-      <p className='text-primary'>this is from mac</p>
-  <div className="card" style={{width: '18rem' }}>
-        <img src="logo.svg" className="card-img-top" alt=""/>
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler nav-toglebtn-shadow bg-success" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item ">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link text-grey-900" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider"/></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
           </div>
-      </div>
+        </div>
+      </nav>
 
     </Fragment>
   )
