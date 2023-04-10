a92e5d40f1d8440be1f9cdfb158f4b57f6d8f99e
Harshenu Mani Tripathi <harsh25796@gmail.com>
2023-04-11 00:10:37 +0530

    login section

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import './style.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
-import { AiFillApple,GiArchaeopteryxFossil } from "react-icons/gi";
+import { AiFillApple, GiArchaeopteryxFossil } from "react-icons/gi";
 
 
 function Home() {
@@ -10,7 +10,7 @@ function Home() {
     <Fragment>
       <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
         <div className="container-fluid">
-          <a className="navbar-brand text-decoration-none lh-1 ms-3 me-5 my-0 px-0 pt-0 fs-3" href="#"><span className='homeicn'><GiArchaeopteryxFossil/></span></a>
+          <a className="navbar-brand text-decoration-none lh-1 ms-3 me-5 my-0 px-0 pt-0 fs-3" href="#"><span className='homeicn'><GiArchaeopteryxFossil /></span></a>
           <button className="navbar-toggler nav-toglebtn-shadow bg-success" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -40,6 +40,46 @@ function Home() {
             <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
+              {/* <!-- Button trigger modal --> */}
+              <button type="button" class="btn btn-outline-success ms-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Login
+              </button>
+
+              {/* <!-- Modal --> */}
+              <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="staticBackdropLabel">Login</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      {/* log in form */}
+                      <form>
+                        <div class="mb-3">
+                          <label for="exampleInputEmail1" class="form-label">Email address</label>
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                          <label for="exampleInputPassword1" class="form-label">Password</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1"/>
+                        </div>
+                        
+                        {/* <div class="mb-3 form-check">
+                          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div> */}
+                        {/* <button type="submit" class="btn btn-primary float-end">login</button> */}
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-outline-success">Login</button>
+                    </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </form>
           </div>
         </div>
