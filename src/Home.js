b7e53bbcd5f286ba2d535d15dd99b7b0b1dd7cc2
Harsh Tripathi <harshtripathi@Harshs-MacBook-Pro.local>
2023-04-11 17:00:58 +0530

    mac

--- src/Home.js
+++ src/Home.js
@@ -40,7 +40,7 @@ function Home() {
             <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
-              {/* <!-- Button trigger modal --> */}
+              {/* <!-- Button trigger modal  --> */}
               <button type="button" class="btn btn-outline-success ms-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 Login
               </button>
