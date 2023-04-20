ea7fa5f58174f5eff20f35d018acdc926847e463
Asheesh Rathour <ashishrathor783@gmail.com>
2023-04-20 05:51:35 +0530

    little more api mod

--- src/components/Axiosapi.js
+++ src/components/Axiosapi.js
@@ -1,24 +1,29 @@
 import axios from 'axios'
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
+export function api(){
+  return(
+    "https://api.unsplash.com/search/photos?query=pussy&per_page=30&orientation=squarish&client_id=1Kc5tj6qtFQj7B0rb5yIO4KQDQ0DT3YivyZBItn37m4"
+  )
+}
 function Axiosapi() {
+  const apilink= api();
   const [datav,dataf] = useState([]);
-  const myfn = ()=>{
-  axios.get("https://api.unsplash.com/search/photos?query=puppies&per_page=30&orientation=landscape&client_id=1Kc5tj6qtFQj7B0rb5yIO4KQDQ0DT3YivyZBItn37m4").then((res)=>{
+  useEffect(()=>{
+  axios.get(apilink).then((res)=>{
     console.log(res.data.results);
     dataf(res.data.results)
 
   })
-}
+},[apilink])
+
   return (
     <Fragment>
-
-        <button className='btn btn-success' type='button' onClick={myfn}>trigger</button>
-        
         <div className='container-fluid'>
           <div className='row'>
             {datav.map((a)=>{
+              var b;
               return(
             <section className='col-md-4 my-2' key={a.id}>
             <div className="card shadow" style={{ boxSizing: 'border-box' }} >
@@ -26,7 +31,13 @@ function Axiosapi() {
                             <div className="card-body">
                                 <h5 className="card-title">{a.user.name}</h5>
                                 <p className="card-text">{a.alt_description}</p>
-                                <Link to="#" className="btn btn-outline-warning">{a.likes}</Link><span className='text-success'>   likes</span>
+                                <button className="btn btn-outline-warning fnpls"  onClick={()=>{
+                                  return (
+                                  b = parseInt(document.getElementsByClassName('fnpls')[[datav.indexOf(a)]].innerHTML),
+                                  document.getElementsByClassName('fnpls')[[datav.indexOf(a)]].innerHTML = b + 1
+
+                                  )
+                                }}>{a.likes}</button><span className='text-success'>   likes</span>
                             </div>
                         </div>
             </section>
--- src/components/Axiosapishow.js
+++ src/components/Axiosapishow.js
@@ -2,56 +2,45 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { api } from "./Axiosapi"
 
 function Axiosapishow() {
-    
-    let { id } = useParams();
-    const [myvar, myfun] = useState([]);
-    const[x,y]=useState({});
-    const [a,b]=useState({});
-    const [c,d]=useState({});
 
-    useEffect(()=>{
-        axios.get("https://api.unsplash.com/search/photos?query=puppies&per_page=30&orientation=landscape&client_id=1Kc5tj6qtFQj7B0rb5yIO4KQDQ0DT3YivyZBItn37m4").then((res)=>{
-            myfun(res.data.results);
-            console.log("add array"); 
-            for( var i=0; i<myvar.length;i++)
-            {
-                if(myvar[i].id === id){
-                    y(myvar[i])
-                    console.log(x)
+    let { id } = useParams();
+    const [myvar, myfun] = useState({});
+    const [x, y] = useState({});
+    const [a, b] = useState({});
+
+
+    useEffect(() => {
+        axios.get(api()).then((res) => {
+            for (var i = 0; i < res.data.results.length; i++) {
+                if (res.data.results[i].id === id) {
+                    myfun(res.data.results[i])
+                    y(res.data.results[i].urls)
+                    b(res.data.results[i].user)
                 }
             }
-           
-            console.log("ok")
-
-
         });
-           
-       
-    },[])
-    
-    
-    
-  
-    
-    
-
+    }, [id])
 
     return (
-       
-       
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='position-relative'>
-                        <img className='position-absolute start-0' alt='oops'  src={x.urls.full}/>
-                        <h1>{x.id}</h1>
-                        {console.log(x.urls.full)}
 
+
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='position-relative col-12 ' >
+                    <div className='py-2'><img src={x.full} className='w-100' alt='Img unavailable!!'/></div>
+                    <div className='position-absolute imgcptnposition rounded p-1 shadow' style={{ backgroundColor: '#a0a0a033' }}>
+                        <div className='dynamic-ftomedium col-12'>{a.name}</div>
+                        <div className='h-6 dynamic-ftosmall col-12'>{myvar.alt_description}</div>
+                        <div className='h-6 dynamic-ftosmall col-12' style={{ color: '#e2e2e2e0' }}>{myvar.description}</div>
                     </div>
+
                 </div>
             </div>
-        
+        </div>
+
 
     )
 }
